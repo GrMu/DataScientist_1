4bb4c1970ffed656bcda59168e290197d164eb1f
Grietus Mulder <grietus.mulder@vito.be>
2024-09-25 19:48:05 +0200

    Alweer een lijn toegevoegd

--- pfd_out_test1.py
+++ pfd_out_test1.py
@@ -2,4 +2,4 @@ print("Deze code komt van Github, test")
 print("Import gelukt")
 print("Deze lijn komt uit PyCharm")
 #commit sessie 2
-print("Deze code komt van PyCharm voor de tweede keer")
+print("Deze code komt van PyCharm voor de 2de keer")
